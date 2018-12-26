a5407390aa70b16e8f751e2df50dd4fa0fd8a4b6
ciscucordo <fcordomi@gmail.com>
2018-12-27 00:22:18 +0100

    primer canvi

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var WebSocketServer = require("ws").Server
-var http = require("http")
+var WebSocketServer = require("ws").Server;
+var http = require("http");
 var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
@@ -15,14 +15,12 @@ var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
 wss.on("connection", function(ws) {
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
-
+  //example
+  ws.send(JSON.stringify(new Date()), function() {  })
+  
   console.log("websocket connection open")
 
   ws.on("close", function() {
     console.log("websocket connection close")
-    clearInterval(id)
   })
 })
