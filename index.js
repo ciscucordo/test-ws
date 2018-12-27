a8da5d504d2fe5225dc14e3ce5655dd9d2a0cfc5
ciscucordo <fcordomi@gmail.com>
2018-12-27 03:46:40 +0100

    4 canvi

--- index.js
+++ index.js
@@ -18,21 +18,21 @@ wss.on("connection", function(ws) {
   
     ws.on('message', function (data) {
         // Broadcast to everyone else.
-        wss.clients.forEach(function each(client) {
-          if (/*client !== ws &&*/ client.readyState === WebSocketServer.OPEN) {
+        // wss.clients.forEach(function each(client) {
+        //  if (/*client !== ws &&*/ client.readyState === WebSocketServer.OPEN) {
             var roomAndNick = split(':', data.roomAndNick);
             var room = roomAndNick[0];
             var nick = roomAndNick[1];
             var chat_msg = data.chat_msg;
             var response_to = '<span><h5>' + nick + '</h5><p>' + chat_msg + '</p><span>data i dia</span></span>';
-            client.send({
+            ws.send({
                 'type': 'chat',
-                'roomAndNick': roomAndNick,
+                'roomAndNick': data.roomAndNick,
                 'msg': response_to
             });
             
-          }
-        });
+        //  }
+        //});
         
         // ws.send('petehant', function() {  })
     });
